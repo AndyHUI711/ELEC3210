4d502de61e3296799cb5c6f4ef39a55a91446fd2
AndyHUI711 <82593344+AndyHUI711@users.noreply.github.com>
2021-12-13 15:13:49 +0800

    Add files via upload

---
+++ src/image_detection/src/image_detection.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+
+import rospy
+import math
+import cv2
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+from visualization_msgs.msg import Marker
+
+
+class ImageDetection:
+    def __init__(self):
+
+        self.subscriber = rospy.Subscriber("/vrep/image", Image, self.callback)
+        self.publisher = rospy.Publisher("image_marker", Marker, queue_size=100)
+
+        # ROS to CV 
+        self.bridge = CvBridge()
+
+        # Load the image
+        path = "/home/user/catkin_ws/src/image_detection/image"
+        pictures_name = ["pic001", "pic002", "pic003", "pic004", "pic005"]
+        self.pictures = [cv2.imread(path+name+".jpg") for name in pictures_name]
+
+        self.surf_cnt = [0] * len(self.pictures)
+        self.square_cnt = [0] * len(self.pictures)
+        self.marked = [False] * len(self.pictures)
+
+        self.bf = cv2.BFMatcher()
+        self.surf = cv2.xfeatures2d.SURF_create(400)
+        self.keypoints, self.descriptors = [], []
+        for i, picture in enumerate(self.pictures):
+            picture = cv2.resize(picture, (400, 400), interpolation=cv2.INTER_AREA)
+            k, d = self.surf.detectAndCompute(picture, None)
+            picture = cv2.flip(picture, 1)
+            kf, df = self.surf.detectAndCompute(picture, None)
+            self.keypoints.append((k, kf))
+            self.descriptors.append((d, df))
+
+        self.markers = []
+        for i in range(len(pictures_name)):
+            marker = Marker()
+            marker.id = i
+            marker.header.frame_id = "/camera_link"
+            marker.type = marker.TEXT_VIEW_FACING
+            marker.action = marker.ADD
+            marker.scale.x = 0.5
+            marker.scale.y = 0.5
+            marker.scale.z = 0.5
+            marker.color.a = 1.0
+            marker.color.r = 1.0
+            marker.color.g = 0.0
+            marker.color.b = 0.0
+            marker.pose.orientation.w = 1.0
+            marker.text = pictures_name[i]
+            self.markers.append(marker)
+
+    def _mark(self, id, img):
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        _, img_BW = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        _, contours, _ = cv2.findContours(img_BW, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+        x, y, w, h = cv2.boundingRect(max(contours, key=cv2.contourArea))
+        ratio = float(w)/h
+
+        if ratio > 1.1 and ratio < 0.5:
+            self.square_cnt[id] = 0
+        else:
+            self.square_cnt[id] += 1
+
+        if (self.square_cnt[id] >= 15):
+            self.markers[id].pose.position.x = 1 / math.tan(math.pi / 8 * h / img.shape[1]) * 0.5
+            self.markers[id].pose.position.y = (x + w/2) / img.shape[1]
+            self.markers[id].pose.position.z = 0
+
+            self.publisher.publish(self.markers[id])
+            self.marked[id] = True
+
+    def _best_fit(self, img):
+        try:
+            best_id = -1
+            best = -1
+            keypoint, descriptor = self.surf.detectAndCompute(img, None)
+
+            for i, d in enumerate(self.descriptors):
+                cur_cnt = 0
+
+                for dd in d:
+                    result = self.bf.knnMatch(dd, descriptor, k=2)
+                    for m, n in result:
+                        if m.distance < 0.75 *n.distance:
+                            cur_cnt += 1
+
+                if cur_cnt > best:
+                    best = cur_cnt
+                    best_id = i
+
+            return best_id, best
+        except Exception as err:
+            return -1, -1
+
+    def callback(self, img):
+        img = self.bridge.imgmsg_to_cv2(img, "bgr8")
+        id, cnt = self._best_fit(img)
+
+        if id == -1:
+            return
+
+        if (cnt < 120):
+            self.surf_cnt[id] = 0
+        else:
+            self.surf_cnt[id] += 1
+
+        if(self.surf_cnt[id] >= 10 and not self.marked[id]):
+            self._mark(id, img)
+
+
+def main():
+    rospy.init_node("image_detection")
+    node = ImageDetection()
+    rospy.spin()
+
+if __name__ == "__main__":
+    main()
